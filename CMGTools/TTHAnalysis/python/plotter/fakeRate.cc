2f9dc787fecd64def4e6219e4f4f1b3c748872f7
peruzzim <peruzzim@server.fake>
2015-05-17 16:22:00 +0200

    fix overwrite fake rate histo issue

--- CMGTools/TTHAnalysis/python/plotter/fakeRate.cc
+++ CMGTools/TTHAnalysis/python/plotter/fakeRate.cc
@@ -18,6 +18,16 @@ TH2 * FR5_el = 0;
 TH2 * QF_el = 0;
 TH2 * FRi_mu[6], *FRi_el[6];
 
+TH2 * FR_mu_FO1_QCD    = 0;
+TH2 * FR_mu_FO1_insitu = 0;
+TH2 * FR_mu_FO2_QCD    = 0;
+TH2 * FR_mu_FO2_insitu = 0;
+TH2 * FR_el_FO1_QCD    = 0;
+TH2 * FR_el_FO1_insitu = 0;
+TH2 * FR_el_FO2_QCD    = 0;
+TH2 * FR_el_FO2_insitu = 0;
+TH2 * FRi_FO_mu[4];
+TH2 * FRi_FO_el[4];
 
 bool loadFRHisto(const std::string &histoName, const char *file, const char *name) {
     TH2 **histo = 0, **hptr2 = 0;
@@ -32,12 +42,23 @@ bool loadFRHisto(const std::string &histoName, const char *file, const char *nam
     else if (histoName == "FR5_mu") { histo = & FR5_mu; hptr2 = & FRi_mu[5]; }
     else if (histoName == "FR5_el") { histo = & FR5_el; hptr2 = & FRi_el[5]; }
     else if (histoName == "QF_el") histo = & QF_el;
+    else if (histoName == "FR_mu_FO1_QCD")  { histo = &FR_mu_FO1_QCD ;  hptr2 = & FRi_FO_mu[0]; }
+    else if (histoName == "FR_mu_FO1_insitu")  { histo = &FR_mu_FO1_insitu ;  hptr2 = & FRi_FO_mu[1]; }
+    else if (histoName == "FR_mu_FO2_QCD")  { histo = &FR_mu_FO2_QCD ;  hptr2 = & FRi_FO_mu[2]; }
+    else if (histoName == "FR_mu_FO2_insitu")  { histo = &FR_mu_FO2_insitu ;  hptr2 = & FRi_FO_mu[3]; }
+    else if (histoName == "FR_el_FO1_QCD")  { histo = &FR_el_FO1_QCD ;  hptr2 = & FRi_FO_el[0]; }
+    else if (histoName == "FR_el_FO1_insitu")  { histo = &FR_el_FO1_insitu ;  hptr2 = & FRi_FO_el[1]; }
+    else if (histoName == "FR_el_FO2_QCD")  { histo = &FR_el_FO2_QCD ;  hptr2 = & FRi_FO_el[2]; }
+    else if (histoName == "FR_el_FO2_insitu")  { histo = &FR_el_FO2_insitu ;  hptr2 = & FRi_FO_el[3]; }
     if (histo == 0)  {
         std::cerr << "ERROR: histogram " << histoName << " is not defined in fakeRate.cc." << std::endl;
         return 0;
     }
 
-    if (*histo != 0) delete *histo;
+    if (*histo != 0) {
+      std::cerr << "WARNING: overwriting histogram " << (*histo)->GetName() << std::endl;
+      delete *histo;
+    }
     TFile *f = TFile::Open(file);
     if (f->Get(name) == 0) {
         std::cerr << "ERROR: could not find " << name << " in " << file << std::endl;
@@ -671,26 +692,29 @@ float fakeRateBin_Muons_pt(float bin) {
 }
 
 
-float fakeRateReader_2lss_FO(float l1eta, float l1pt, float l2eta, float l2pt, int l1pdgId, int l2pdgId, int pass1, int pass2)
+float fakeRateReader_2lss_FO(float l1eta, float l1pt, float l2eta, float l2pt, int l1pdgId, int l2pdgId, int pass1, int pass2, int fo12, int isinsitu)
 {
+  assert (fo12==1 || fo12==2);
+  assert (isinsitu==0 || isinsitu==1);
+  int ind = 2*(fo12-1)+isinsitu;
   int nfail = 2-pass1-pass2;
     switch (nfail) {
         case 1: {
             double fpt,feta; int fid;
             if (pass2)   { fpt = l1pt; feta = std::abs(l1eta); fid = abs(l1pdgId); }
             else         { fpt = l2pt; feta = std::abs(l2eta); fid = abs(l2pdgId); }
-            TH2 *hist = (fid == 11 ? FR_el : FR_mu);
+            TH2 *hist = (fid == 11 ? FRi_FO_el[ind] : FRi_FO_mu[ind]);
             int ptbin  = std::max(1, std::min(hist->GetNbinsX(), hist->GetXaxis()->FindBin(fpt)));
             int etabin = std::max(1, std::min(hist->GetNbinsY(), hist->GetYaxis()->FindBin(feta)));
             double fr = hist->GetBinContent(ptbin,etabin);
             return fr/(1-fr);
         }
         case 2: {
-            TH2 *hist1 = (abs(l1pdgId) == 11 ? FR_el : FR_mu);
+            TH2 *hist1 = (abs(l1pdgId) == 11 ? FRi_FO_el[ind] : FRi_FO_mu[ind]);
             int ptbin1  = std::max(1, std::min(hist1->GetNbinsX(), hist1->GetXaxis()->FindBin(l1pt)));
             int etabin1 = std::max(1, std::min(hist1->GetNbinsY(), hist1->GetYaxis()->FindBin(std::abs(l1eta))));
             double fr1 = hist1->GetBinContent(ptbin1,etabin1);
-            TH2 *hist2 = (abs(l2pdgId) == 11 ? FR_el : FR_mu);
+            TH2 *hist2 = (abs(l2pdgId) == 11 ? FRi_FO_el[ind] : FRi_FO_mu[ind]);
             int ptbin2  = std::max(1, std::min(hist2->GetNbinsX(), hist2->GetXaxis()->FindBin(l2pt)));
             int etabin2 = std::max(1, std::min(hist2->GetNbinsY(), hist2->GetYaxis()->FindBin(std::abs(l2eta))));
             double fr2 = hist2->GetBinContent(ptbin2,etabin2);
