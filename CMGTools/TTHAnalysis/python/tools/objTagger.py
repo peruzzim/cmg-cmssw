04966482219026b81eae9482b232cce83d4e05cd
peruzzim <peruzzim@server.fake>
2015-05-20 15:17:10 +0200

    changed obj tagging logic to avoid double indices in plotting

--- CMGTools/TTHAnalysis/python/tools/objTagger.py
+++ CMGTools/TTHAnalysis/python/tools/objTagger.py
@@ -1,19 +1,19 @@
 from CMGTools.TTHAnalysis.treeReAnalyzer import *
 
 class ObjTagger:
-    def __init__(self,label,coll,sel,sizelimit=20):
-        self.label = "" if (label in ["",None]) else ("_"+label)
+    def __init__(self,label,coll,sel,sizelimit=10):
+        self.label = "" if (label in ["",None]) else (label)
         self.coll = coll
         self.sel = sel
         self.sizelimit = sizelimit
     def listBranches(self):
-        biglist = [ ("n"+self.coll+self.label, "I"), ("i"+self.coll+self.label,"I",self.sizelimit,"n"+self.coll+self.label) ]
+        biglist = [ ("n"+self.coll+"_"+self.label, "I"), (self.coll+"_is"+self.label,"I",self.sizelimit,"n"+self.coll) ]
         return biglist
     def __call__(self,event):
         objs = [l for l in Collection(event,self.coll,"n"+self.coll)]
         ret = {};
-        ret["n"+self.coll+self.label]=0
-        ret["i"+self.coll+self.label]=[]
+        ret["n"+self.coll+"_"+self.label]=0
+        ret[self.coll+"_is"+self.label]=[]
         for i,ob in enumerate(objs):
             ispassing = True
             for selector in self.sel:
@@ -21,11 +21,11 @@ class ObjTagger:
                     ispassing = False
                     break
             if ispassing:
-                if ret["n"+self.coll+self.label] == self.sizelimit:
+                if ret["n"+self.coll+"_"+self.label] == self.sizelimit:
                     print 'WARNING: reached branch size limit for ObjTagger labeled '+self.label
                     break
-                ret["n"+self.coll+self.label] += 1
-                ret["i"+self.coll+self.label].append(i)
+                ret["n"+self.coll+"_"+self.label] += 1
+            ret[self.coll+"_is"+self.label].append(1 if ispassing else 0)
         return ret
 
 if __name__ == '__main__':
