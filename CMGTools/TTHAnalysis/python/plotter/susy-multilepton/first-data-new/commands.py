3c670552a225b5c77cde82b035b90b4bf013a74e
peruzzim <peruzzim@users.noreply.github.com>
2015-10-20 14:02:10 +0200

    plotting

--- CMGTools/TTHAnalysis/python/plotter/susy-multilepton/first-data-new/commands.py
+++ CMGTools/TTHAnalysis/python/plotter/susy-multilepton/first-data-new/commands.py
@@ -10,11 +10,18 @@ COMMOPT='--s2v --tree treeProducerSusyMultilepton --FMC sf/t {P}/1_puWeights_v1_
 
 SELECTIONS=["ZtoEE","ZtoMuMu","ttbar","Wl","Zl","ttbar_semiLeptonic"]
 
+
 for SEL in SELECTIONS:
     print '#'+SEL
     MCA = 'mca_13tev_%s.txt'%SEL if SEL in ['Wl','Zl','ttbar_semiLeptonic'] else 'mca_13tev.txt'
     MYMCC = '--mcc %s/mcc_%s.txt'%(WDIR,SEL)
 #    print '%s %s/%s %s/cuts_%s.txt %s/plots_%s.txt %s -P %s -l %s %s --scaleSigToData --pdir %s/%s/ScaleSigToData'%(EXE,WDIR,MCA,WDIR,SEL,WDIR,SEL,COMMOPT,MYTREEDIR,MYLUMI,MYMCC,ODIR,SEL)
-    print '%s %s/%s %s/cuts_%s.txt %s/plots_lepquantities.txt %s -P %s -l %s %s --pdir %s/%s/ScaleToLumi'%(EXE,WDIR,MCA,WDIR,SEL,WDIR,COMMOPT,MYTREEDIR,MYLUMI,MYMCC,ODIR,SEL)
-#    print '%s %s/%s %s/cuts_%s.txt %s/plots_eventquantities.txt %s -P %s -l %s %s --pdir %s/%s/ScaleToLumi'%(EXE,WDIR,MCA,WDIR,SEL,WDIR,COMMOPT,MYTREEDIR,MYLUMI,MYMCC,ODIR,SEL)
+    COARSE = '_coarse' if SEL in ['Wl','Zl','ttbar_semiLeptonic'] else ''
+    print '%s %s/%s %s/cuts_%s.txt %s/plots_lepquantities%s.txt %s -P %s -l %s %s --pdir %s/%s/ScaleToLumi'%(EXE,WDIR,MCA,WDIR,SEL,WDIR,COARSE,COMMOPT,MYTREEDIR,MYLUMI,MYMCC,ODIR,SEL)
 
+
+SELECTIONS=["ttbar_application"]
+for SEL in SELECTIONS:
+    print '#'+SEL
+    MCA = 'mca_13tev.txt'
+    print '%s %s/%s %s/cuts_%s.txt %s/plots_eventquantities.txt %s -P %s -l %s --pdir %s/%s/ScaleToLumi'%(EXE,WDIR,MCA,WDIR,SEL,WDIR,COMMOPT,MYTREEDIR,MYLUMI,ODIR,SEL)
