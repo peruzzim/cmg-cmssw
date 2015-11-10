06e6641f1107a5dfe8d4472c9ab6984605ea678c
peruzzim <peruzzim@users.noreply.github.com>
2015-11-10 16:09:00 +0100

    Utility scripts

--- CMGTools/TTHAnalysis/python/plotter/fractionCutTree.py
+++ CMGTools/TTHAnalysis/python/plotter/fractionCutTree.py
@@ -1,19 +1,20 @@
 import ROOT
 import os, sys
 
-# usage: python verifyFTree BIGTREE_DIR FTREE_DIR DATASET_NAME ...
+# usage: python fractionCutTree.py BIGTREE_DIR FTREE_DIR CUT_STRING DATASET_NAME ...
 
-dsets = sys.argv[3:]
-if len(sys.argv)<4:
+dsets = sys.argv[4:]
+if len(sys.argv)<5:
     dsets = [d.replace('evVarFriend_','').replace('.root','') for d in os.listdir(sys.argv[2]) if 'evVarFriend' in d]
 
 for dset in dsets:
     f_t = ROOT.TFile.Open(sys.argv[1]+'/'+dset+'/treeProducerSusyMultilepton/tree.root')
     t_t = f_t.Get("tree")
-    n_t = t_t.GetEntries()
-    f_t.Close()
     f_f = ROOT.TFile.Open(sys.argv[2]+'/evVarFriend_'+dset+'.root')
     t_f = f_f.Get("sf/t")
-    n_f = t_f.GetEntries()
+    t_t.AddFriend(t_f)
+    n_pass = t_t.GetEntries(sys.argv[3])
+    n_tot = t_t.GetEntries()
+    f_t.Close()
     f_f.Close()
-    print '%s: %d - %d : %s'%(dset,n_t,n_f,'OK' if n_t==n_f else 'ERROR '*15+' !!!')
+    if n_pass!=0:    print '%s: %d out of %d : %.2f%%'%(dset,n_pass,n_tot,float(n_pass)/n_tot*100 if n_tot!=0 else 0)
