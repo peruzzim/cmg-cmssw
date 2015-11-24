f12058c4a3332793cb945f6fbf50fe0630dd2c11
peruzzim <peruzzim@users.noreply.github.com>
2015-11-24 18:07:33 +0100

    verify trees on eos

--- CMGTools/TTHAnalysis/macros/verifyFTree.py
+++ CMGTools/TTHAnalysis/macros/verifyFTree.py
@@ -7,12 +7,22 @@ dsets = sys.argv[3:]
 if len(sys.argv)<4:
     dsets = [d.replace('evVarFriend_','').replace('.root','') for d in os.listdir(sys.argv[2]) if 'evVarFriend' in d]
 
+def openRootOrUrl(myfile):
+    _f_t = None
+    if os.path.exists(myfile):
+        _f_t = ROOT.TFile.Open(myfile)
+    elif os.path.exists(myfile+'.url'):
+        with open(myfile+'.url','r') as urlf:
+            myfile = urlf.readline().replace('\n','')
+            _f_t = ROOT.TXNetFile(myfile)
+    return _f_t
+
 for dset in dsets:
-    f_t = ROOT.TFile.Open(sys.argv[1]+'/'+dset+'/treeProducerSusyMultilepton/tree.root')
+    f_t = openRootOrUrl(sys.argv[1]+'/'+dset+'/treeProducerSusyMultilepton/tree.root')
     t_t = f_t.Get("tree")
     n_t = t_t.GetEntries()
     f_t.Close()
-    f_f = ROOT.TFile.Open(sys.argv[2]+'/evVarFriend_'+dset+'.root')
+    f_f = openRootOrUrl(sys.argv[2]+'/evVarFriend_'+dset+'.root')
     t_f = f_f.Get("sf/t")
     n_f = t_f.GetEntries()
     f_f.Close()
