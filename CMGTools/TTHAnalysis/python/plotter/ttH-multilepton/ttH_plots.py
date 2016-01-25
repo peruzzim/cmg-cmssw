33c7c5c663169c023f384bbde12967e66db28f8e
peruzzim <peruzzim@users.noreply.github.com>
2016-01-25 23:57:18 +0100

    tth plotting work in progress

--- CMGTools/TTHAnalysis/python/plotter/ttH-multilepton/ttH_plots.py
+++ CMGTools/TTHAnalysis/python/plotter/ttH-multilepton/ttH_plots.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
-ODIR="test_tthplots_jan20"
+ODIR="test_tthplots_jan25"
 
 lumi = 2.16
 
 def base(selection='2lss'):
 
     T="-P /data/p/peruzzi/TREES_74X_140116_MiniIso_tauClean_Mor16lepMVA"
-    CORE="%s --neg --s2v --tree treeProducerSusyMultilepton --mcc ttH-multilepton/lepchoice-ttH-FO.txt --mcc ttH-multilepton/ttH_2lss3l_triggerdefs.txt -F sf/t {P}/2_recleaner_v2/evVarFriend_{cname}.root"%T
+    CORE="%s --neg --s2v --tree treeProducerSusyMultilepton --mcc ttH-multilepton/lepchoice-ttH-FO.txt --mcc ttH-multilepton/ttH_2lss3l_triggerdefs.txt -F sf/t {P}/2_recleaner_v3_ttHvariations/evVarFriend_{cname}.root"%T
 
     if selection=='2lss':
-        GO="python mcPlots.py %s ttH-multilepton/mca-2lss-Mor16.txt ttH-multilepton/2lss_Mor16.txt -f -j 8 --lspam '#bf{CMS} #it{Preliminary}' --legendWidth 0.20 --legendFontSize 0.035 --showRatio --maxRatioRange 0 3 ttH-multilepton/cr_2lss_lowj_plots.txt --showMCError"%CORE
-    elif selection=='3l':
-        GO="python mcPlots.py %s ttH-multilepton/mca-3l-Mor16.txt ttH-multilepton/3l_Mor16.txt -f -j 8 --lspam '#bf{CMS} #it{Preliminary}' --legendWidth 0.20 --legendFontSize 0.035 --showRatio --maxRatioRange 0 3 ttH-multilepton/cr_3l_plots.txt --showMCError"%CORE
+        GO="python mcPlots.py %s ttH-multilepton/mca-2lss-Mor16.txt ttH-multilepton/2lss_Mor16.txt -f -j 8 --lspam '#bf{CMS} #it{Preliminary}' --legendWidth 0.20 --legendFontSize 0.035 --showRatio --maxRatioRange 0 3 ttH-multilepton/check_FO_def_plots.txt --showMCError"%CORE
+#    elif selection=='3l':
+#        GO="python mcPlots.py %s ttH-multilepton/mca-3l-Mor16.txt ttH-multilepton/3l_Mor16.txt -f -j 8 --lspam '#bf{CMS} #it{Preliminary}' --legendWidth 0.20 --legendFontSize 0.035 --showRatio --maxRatioRange 0 3 ttH-multilepton/cr_3l_plots.txt --showMCError"%CORE
     else:
         raise RuntimeError, 'Unknown selection'
 
@@ -35,13 +35,40 @@ def setwide(x):
 
 if __name__ == '__main__':
 
-    x = base('2lss')
-#    x = procs(x,['ttH'])
-    runIt(x,'2lss_SR')
 
-    x = base('3l')
+#    x = base('2lss')
 #    x = procs(x,['ttH'])
-    runIt(x,'3l_SR')
+#    x = add(x,"-R mt2FO_2Tight mt2FO_mt2Tight 'nLepFO>=2 && nLepTight>=2'")
+#    x = add(x, "-A alwaystrue noHtautau 'GenHiggsDecayMode!=15'")
+#    runIt(x,'2lss_SR')
+#    x2 = add(x,"-X 4j -X 2b1B")
+#    runIt(x2,'2lss_SR_nojetcut')
+#
+#    x3 = add(x2,"-R pt2010 pt2015 'LepGood1_conePt>20 && LepGood2_conePt>15' ")
+#    runIt(x3,'2lss_SR_nojetcut_cut15')
+#
+#    for mod in ["ReclConept15","ReclBtagTightVeto","ReclBtagMediumVeto"]:
+#
+#        x = base('2lss')
+#        x = procs(x,['ttH'])
+#        x = add(x,"-R mt2FO_2Tight mt2FO_mt2Tight 'nLepFO>=2 && nLepTight>=2'")
+#        x = add(x, "-A alwaystrue noHtautau 'GenHiggsDecayMode!=15'")
+#        x = x.replace('lepchoice-ttH-FO.txt','lepchoice-ttH-FO-%s.txt'%mod)
+#        runIt(x,'2lss_SR_%s'%mod)
+#        x2 = add(x,"-X 4j -X 2b1B")
+#        runIt(x2,'2lss_SR_nojetcut_%s'%mod)
+
+    x = base('2lss')
+    x = x.replace('mca-2lss-Mor16.txt','mca-2lss-Mor16-appl.txt')
+    x = x.replace('2_recleaner_v3_ttHvariations','2_recleaner_v3')
+    runIt(x,'2lss_SR_appl')
+
+
+
+
+#    x = base('3l')
+#    x = procs(x,['ttH'])
+#    runIt(x,'3l_SR')
 
 
 
